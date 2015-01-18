ac12fa3b27381d70da550594bd2d78c1657382b3
Edham Arief Dawillah / edthix <edthix@gmail.com>
2015-01-18 12:03:30 +0800

    Added dependencies
    net.mikera/core.matrix
    clatrix

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://dawillah.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.mikera/core.matrix "0.32.1"]
+                 [clatrix "0.4.0"]]
   :main ^:skip-aot ml.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
